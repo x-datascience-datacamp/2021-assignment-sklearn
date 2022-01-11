53abee2d4006509cf48a6df073defcfc49201b0a
Ayoub <Ayoub.TABAAI@emines.um6p.ma>
2022-01-11 21:03:53 +0100

    Up my solution

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -1,12 +1,9 @@
 """Assignment - making a sklearn estimator and cv splitter.
-
 The goal of this assignment is to implement by yourself:
-
 - a scikit-learn estimator for the KNearestNeighbors for classification
-  tasks and check that it is working properly.
+tasks and check that it is working properly.
 - a scikit-learn CV splitter where the splits are based on a Pandas
-  DateTimeIndex.
-
+DateTimeIndex.
 Detailed instructions for question 1:
 The nearest neighbor classifier predicts for a point X_i the target y_k of
 the training sample X_k which is the closest to X_i. We measure proximity with
@@ -18,7 +15,6 @@ repo `pytest test_sklearn_questions.py`. Note that to be fully valid, a
 scikit-learn estimator needs to check that the input given to `fit` and
 `predict` are correct using the `check_*` functions imported in the file.
 Make sure to use them to pass `test_nearest_neighbor_check_estimator`.
-
 Detailed instructions for question 2:
 The data to split should contain the index or one column in
 datatime format. Then the aim is to split the data between train and test
@@ -27,25 +23,19 @@ predict of the following. For example if you have data distributed from
 november 2020 to march 2021, you have have 5 splits. The first split
 will allow to learn on november data and predict on december data, the
 second split to learn december and predict on january etc.
-
 We also ask you to respect the pep8 convention: https://pep8.org. This will be
 enforced with `flake8`. You can check that there is no flake8 errors by
 calling `flake8` at the root of the repo.
-
 Finally, you need to write docstrings for the methods you code and for the
 class. The docstring will be checked using `pydocstyle` that you can also
 call at the root of the repo.
-
 Hints
 -----
 - You can use the function:
-
 from sklearn.metrics.pairwise import pairwise_distances
-
 to compute distances between 2 sets of samples.
 """
 import numpy as np
-import pandas as pd
 
 from sklearn.base import BaseEstimator
 from sklearn.base import ClassifierMixin
@@ -60,7 +50,6 @@ from sklearn.metrics.pairwise import pairwise_distances
 
 class KNearestNeighbors(BaseEstimator, ClassifierMixin):
     """KNearestNeighbors classifier."""
-
     def __init__(self, n_neighbors=1):  # noqa: D107
         self.n_neighbors = n_neighbors
 
@@ -73,12 +62,18 @@ class KNearestNeighbors(BaseEstimator, ClassifierMixin):
             training data.
         y : ndarray, shape (n_samples,)
             target values.
-
         Returns
         ----------
         self : instance of KNearestNeighbors
             The current instance of the classifier
         """
+        # Check the features and the labels
+        X, y = check_X_y(X, y)
+        check_classification_targets(y)
+        # Check arrays
+        X = check_array(X)
+        self.features_ = X
+        self.labels_ = y
         return self
 
     def predict(self, X):
@@ -94,25 +89,36 @@ class KNearestNeighbors(BaseEstimator, ClassifierMixin):
         y : ndarray, shape (n_test_samples,)
             Class labels for each test data sample.
         """
+        # check that the model is fitted
+        check_is_fitted(self)
+        X = check_array(X)
         y_pred = np.zeros(X.shape[0])
+        n_samples, n_features = X.shape
+        for i in range(n_samples):
+            x = X[i, :]
+            distances = pairwise_distances(self.features_,
+                                           x.reshape((1,
+                                                      n_features))).flatten()
+            knn = np.argsort(distances)[:self.n_neighbors]
+            nearest_labels = self.labels_[knn]
+            y_pred[i] = np.bincount(nearest_labels).argmax()
         return y_pred
 
     def score(self, X, y):
         """Calculate the score of the prediction.
-
         Parameters
         ----------
         X : ndarray, shape (n_samples, n_features)
             training data.
         y : ndarray, shape (n_samples,)
             target values.
-
         Returns
         ----------
         score : float
             Accuracy of the model computed for the (X, y) pairs.
         """
-        return 0.
+        y_pred = self.predict(X)
+        return np.mean(y_pred == y)
 
 
 class MonthlySplit(BaseCrossValidator):
@@ -152,11 +158,18 @@ class MonthlySplit(BaseCrossValidator):
         n_splits : int
             The number of splits.
         """
-        return 0
+        X_resset = X.reset_index()
+        if X_resset[self.time_col].dtype != "datetime64[ns]":
+            raise ValueError('Not datetime')
+        n_splits = 12*(X_resset[self.time_col].max().year -
+                       X_resset[self.time_col].min().year)
+        n_splits += X_resset[self.time_col].max().month - X_resset[
+            self.time_col].min().month
+
+        return n_splits
 
     def split(self, X, y, groups=None):
         """Generate indices to split data into training and test set.
-
         Parameters
         ----------
         X : array-like of shape (n_samples, n_features)
@@ -166,7 +179,6 @@ class MonthlySplit(BaseCrossValidator):
             Always ignored, exists for compatibility.
         groups : array-like of shape (n_samples,)
             Always ignored, exists for compatibility.
-
         Yields
         ------
         idx_train : ndarray
@@ -174,12 +186,15 @@ class MonthlySplit(BaseCrossValidator):
         idx_test : ndarray
             The testing set indices for that split.
         """
-
-        n_samples = X.shape[0]
         n_splits = self.get_n_splits(X, y, groups)
+        x = X.sort_index()
+        x = X.reset_index()
+        months = x[self.time_col].dt.to_period('M').unique()
         for i in range(n_splits):
-            idx_train = range(n_samples)
-            idx_test = range(n_samples)
+            idx_train = x.index[x[self.time_col].dt.to_period(
+                                    'M') == months[i]]
+            idx_test = x.index[x[self.time_col].dt.to_period(
+                'M') == months[i+1]]
             yield (
                 idx_train, idx_test
             )
