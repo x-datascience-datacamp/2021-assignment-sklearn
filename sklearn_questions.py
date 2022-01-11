d62c5cb6e27d2bf7b948eae3d0f410bf98655d22
Ali Bellamine <contact@alibellamine.me>
2022-01-11 22:48:35 +0100

    Enfin !!! :D

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -57,11 +57,23 @@ from sklearn.utils.validation import check_array
 from sklearn.utils.multiclass import check_classification_targets
 from sklearn.metrics.pairwise import pairwise_distances
 
+from collections import Counter
+
 
 class KNearestNeighbors(BaseEstimator, ClassifierMixin):
     """KNearestNeighbors classifier."""
 
     def __init__(self, n_neighbors=1):  # noqa: D107
+        """
+            KNearestNeighbors
+
+            Parameters
+            ---------
+            n_neighbors : number of neighbors to consider
+        """
+        if (isinstance(n_neighbors, int) is False):
+            raise ValueError("provide an integer please")
+
         self.n_neighbors = n_neighbors
 
     def fit(self, X, y):
@@ -79,6 +91,19 @@ class KNearestNeighbors(BaseEstimator, ClassifierMixin):
         self : instance of KNearestNeighbors
             The current instance of the classifier
         """
+
+        X, y = check_X_y(X, y)
+        X = check_array(X)
+        y = check_array(y, ensure_2d=False, dtype=None)
+        check_classification_targets(y)
+
+        # Just storing the data for distance calculation
+        self.X_ = X
+        self.y_ = dict(zip(range(len(y)), y))
+        self.classes_ = np.unique(y).tolist()
+
+        self.n_features_in_ = X.shape[1]
+
         return self
 
     def predict(self, X):
@@ -94,7 +119,25 @@ class KNearestNeighbors(BaseEstimator, ClassifierMixin):
         y : ndarray, shape (n_test_samples,)
             Class labels for each test data sample.
         """
+
+        check_is_fitted(self)
+        X = check_array(X)
+
         y_pred = np.zeros(X.shape[0])
+        distance = pairwise_distances(X, self.X_, metric="euclidean")
+        if distance.shape[1] >= self.n_neighbors:
+            n_neighbors = self.n_neighbors
+        else:
+            n_neighbors = (distance.shape[0]-1)
+
+        argmins = np.argpartition(distance,
+                                  n_neighbors, axis=1)[:, :n_neighbors]
+
+        # Now, vote
+        labels = np.vectorize(self.y_.get)(argmins)
+        y_pred = np.apply_along_axis(lambda x: max(Counter(x),
+                                     key=np.where), 1, labels)
+
         return y_pred
 
     def score(self, X, y):
@@ -112,7 +155,16 @@ class KNearestNeighbors(BaseEstimator, ClassifierMixin):
         score : float
             Accuracy of the model computed for the (X, y) pairs.
         """
-        return 0.
+
+        X, y = check_X_y(X, y)
+        X = check_array(X)
+        y = check_array(y, ensure_2d=False)
+        check_classification_targets(y)
+
+        y_hat = self.predict(X)
+        score = np.mean(y == y_hat)
+
+        return score
 
 
 class MonthlySplit(BaseCrossValidator):
@@ -134,6 +186,63 @@ class MonthlySplit(BaseCrossValidator):
     def __init__(self, time_col='index'):  # noqa: D107
         self.time_col = time_col
 
+    def _get_date_column(self, X):
+        """
+            _get_combinations :
+                Compute the month - year combinations from the date column
+            Parameters :
+            ------------
+            X : array-like of shape (n_samples, n_features)
+            Training data, where `n_samples` is the number of samples
+            and `n_features` is the number of features.
+
+            Return :
+            --------
+            Pandas Series of the date column
+        """
+
+        if (self.time_col != 'index') and (self.time_col in X.columns):
+            date_column = X[self.time_col]
+        elif self.time_col == 'index':
+            date_column = X.reset_index()['index']
+        else:
+            raise Exception
+
+        # Check the type
+        if pd.api.types.is_datetime64_any_dtype(date_column) is False:
+            raise ValueError("The column should be in datetime format.")
+
+        return date_column
+
+    def _get_combinations(self, X):
+        """
+            _get_combinations :
+                Compute the month - year combinations from the date column
+            Parameters :
+            ------------
+
+            X : array-like of shape (n_samples, n_features)
+            Training data, where `n_samples` is the number of samples
+            and `n_features` is the number of features.
+
+            Returns :
+            --------
+            list of tuples of year and month combinations
+        """
+
+        date_column = self._get_date_column(X)
+
+        month_year = list(
+            zip(
+                date_column.dt.year.values.tolist(),
+                date_column.dt.month.values.tolist()
+            )
+        )
+        month_year_unique = list(set(month_year))
+        month_year_unique.sort()
+
+        return month_year_unique
+
     def get_n_splits(self, X, y=None, groups=None):
         """Return the number of splitting iterations in the cross-validator.
 
@@ -152,7 +261,12 @@ class MonthlySplit(BaseCrossValidator):
         n_splits : int
             The number of splits.
         """
-        return 0
+
+        month_year_unique = self._get_combinations(X)
+
+        n_splits = len(month_year_unique)-1
+
+        return n_splits
 
     def split(self, X, y, groups=None):
         """Generate indices to split data into training and test set.
@@ -175,11 +289,20 @@ class MonthlySplit(BaseCrossValidator):
             The testing set indices for that split.
         """
 
-        n_samples = X.shape[0]
         n_splits = self.get_n_splits(X, y, groups)
+        combinations = self._get_combinations(X)
+
+        date_column = self._get_date_column(X)
+        year_col = date_column.dt.year
+        month_col = date_column.dt.month
+
         for i in range(n_splits):
-            idx_train = range(n_samples)
-            idx_test = range(n_samples)
+            year, month = combinations[i]
+            next_year, next_month = combinations[i+1]
+
+            idx_train = np.where((month_col == month) & (year_col == year))
+            idx_test = np.where((month_col == next_month)
+                                & (year_col == next_year))
             yield (
                 idx_train, idx_test
             )
