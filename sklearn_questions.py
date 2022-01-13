a39c5a7bf25ab83d51e20020703c740822307238
Zongmin741 <zongmin.li@polytechnique.edu>
2022-01-14 00:07:28 +0100

    UP my solution2

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -201,7 +201,6 @@ class MonthlySplit(BaseCrossValidator):
         idx_test : ndarray
             The testing set indices for that split.
         """
-
         n_splits = self.get_n_splits(X, y, groups)
         if self.time_col != 'index':
             X = X.set_index(self.time_col)
