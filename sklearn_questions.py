2b4210f2eff5922eb1e821b448c23b7438da6b2c
Abdallah0799 <tornadogazal@gmail.com>
2022-01-13 16:02:42 +0100

    Update sklearn_questions.py

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -150,6 +150,7 @@ class MonthlySplit(BaseCrossValidator):
         for which this column is not a datetime, it will raise a ValueError.
         To use the index as column just set `time_col` to `'index'`.
     """
+    
     def __init__(self, time_col='index'):  # noqa: D107
         self.time_col = time_col
 
@@ -200,7 +201,6 @@ class MonthlySplit(BaseCrossValidator):
         idx_test : ndarray
             The testing set indices for that split.
         """
-
         X = X.reset_index()
 
         time_values = X[self.time_col].sort_values()
